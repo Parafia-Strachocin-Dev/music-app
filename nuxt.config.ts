f17419f5268fbaf46779990e0209f34a8d5692ad
Dominik Tomaszewski <domcio.tomcio@gmail.com>
2026-03-13 12:50:37 +0100

    Refactor code structure for improved readability and maintainability

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,14 +3,53 @@ export default defineNuxtConfig({
   app: {
     head: {
       link: [
-        { rel: 'icon', type: 'image/png', href: '/icon.png' }
+        { rel: 'icon', type: 'image/png', href: '/icon.png' },
+        { rel: 'apple-touch-icon', href: '/icon.png' }
       ]
     }
   },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: [
-    '@nuxt/ui'
+    '@nuxt/ui',
+    '@vite-pwa/nuxt'
   ],
-  css: ['~/assets/css/main.css']
+  css: ['~/assets/css/main.css'],
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      id: '/',
+      name: 'Uwielbiajmy GO!',
+      short_name: 'Uwielbiajmy GO!',
+      description: 'Song app for Christian musicians.',
+      theme_color: '#09090B',
+      background_color: '#09090B',
+      display: 'standalone',
+      lang: 'pl',
+      start_url: '/',
+      icons: [
+        {
+          src: '/pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        },
+        {
+          src: '/pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable'
+        }
+      ]
+    },
+    devOptions: {
+      enabled: true,
+      type: 'module',
+      suppressWarnings: true
+    }
+  }
 })
